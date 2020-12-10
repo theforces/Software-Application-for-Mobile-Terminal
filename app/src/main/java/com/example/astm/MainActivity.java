2dd782664f6f88c7734ef268d82573fafa4ec683
Moise Andrei <moiseandrei2007@yahoo.com>
2020-12-10 16:55:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/astm/MainActivity.java
@@ -0,0 +1,562 @@
+package com.example.astm;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect2d;
+import org.opencv.core.Point;
+import org.opencv.core.Rect2d;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 102;
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+    //    int counter = 0; // variable used for mirroring screen in Grayscale
+//    boolean startCanny = false; //variable used for Edge Detection View
+    boolean startYolo = false;
+    boolean firstTImeYolo = false;
+
+    boolean startFaces = false;
+    boolean firstTimeFaces = false;
+
+    Net tinyYolo;
+    Net detector;
+
+    Button facesButton;
+    Button objectsButton;
+    Button swapCameraButton;
+
+    private int mCameraId = 0;
+
+    // Edge Detection Function for the View
+/*    public void Canny(View Button){
+
+        if (startCanny == false){
+
+            startCanny = true;
+
+        }
+
+        else{
+
+            startCanny = false;
+
+        }
+    }*/
+
+    public void Yolo(View Button) {
+
+        if (!startYolo) {
+
+            startYolo = true;
+
+            objectsButton.setText("Objects: ON");
+
+            facesButton.setEnabled(false);
+
+            swapCameraButton.setEnabled(false);
+
+            if (!firstTImeYolo) {
+
+                String tinyYoloCfg = getApplicationContext().getFilesDir().getPath() + "/dnns/yolov3-tiny.cfg";
+                String tinyYoloWeights = getApplicationContext().getFilesDir().getPath() + "/dnns/yolov3-tiny.weights";
+
+                tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+
+            }
+        } else {
+            startYolo = false;
+
+            objectsButton.setText("Objects");
+
+            facesButton.setEnabled(true);
+
+            swapCameraButton.setEnabled(true);
+        }
+
+    }
+
+
+    public void Faces(View Button) {
+
+        if (!startFaces) {
+
+            startFaces = true;
+
+            facesButton.setText("Faces: ON");
+
+            objectsButton.setEnabled(false);
+
+            swapCameraButton.setEnabled(false);
+
+            if (!firstTimeFaces) {
+
+                firstTimeFaces = true;
+                String protoPath = getApplicationContext().getFilesDir().getPath() + "/dnns/deploy.prototxt";
+                String caffeWeights = getApplicationContext().getFilesDir().getPath() + "/dnns/res10_300x300_ssd_iter_140000.caffemodel";
+
+                detector = Dnn.readNetFromCaffe(protoPath, caffeWeights);
+
+            }
+
+
+        } else {
+
+            startFaces = false;
+
+            facesButton.setText("Faces");
+
+            objectsButton.setEnabled(true);
+
+            swapCameraButton.setEnabled(true);
+
+        }
+
+    }
+
+
+    public void swapCamera(View Button) {
+        mCameraId = mCameraId^1; //bitwise not operation to flip 1 to 0 and vice versa
+        cameraBridgeViewBase.disableView();
+        cameraBridgeViewBase.setCameraIndex(mCameraId);
+        cameraBridgeViewBase.enableView();
+    }
+
+    public void copyFileOrDir(String path) {
+        AssetManager assetManager = this.getAssets();
+        String[] assets = null;
+        try {
+            assets = assetManager.list(path);
+            if (assets.length == 0) {
+                copyFile(path);
+            } else {
+                String fullPath = getApplicationContext().getFilesDir().getPath() + "/" + path;
+                File dir = new File(fullPath);
+                if (!dir.exists())
+                    dir.mkdir();
+                for (int i = 0; i < assets.length; ++i) {
+                    copyFileOrDir(path + "/" + assets[i]);
+                }
+            }
+        } catch (IOException ex) {
+            Log.e("tag", "I/O Exception", ex);
+        }
+    }
+
+    private void copyFile(String filename) {
+        AssetManager assetManager = this.getAssets();
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(filename);
+            String newFileName = getApplicationContext().getFilesDir().getPath() + "/" + filename;
+            out = new FileOutputStream(newFileName);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+        } catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        }
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            copyFileOrDir("dnns");
+        }
+
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.CameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCameraIndex(mCameraId);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        facesButton = findViewById(R.id.faces);
+        objectsButton = findViewById(R.id.objects);
+        swapCameraButton = findViewById(R.id.swapCamera);
+
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+
+                switch (status) {
+
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+
+
+            }
+
+        };
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        Mat frame = inputFrame.rgba();
+
+        // Used for mirroring screen in Grayscale
+
+       /* if (counter % 2 == 0){
+
+            Core.flip(frame, frame, 1);
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+
+
+        }
+
+        counter = counter + 1;*/
+        // End of mirroring screen in Grayscale
+
+
+        // Edge Detection View
+
+        /*if (startCanny == true){
+
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+            Imgproc.Canny(frame, frame, 100, 80);
+
+        }*/
+        // End of Edge Detection View
+
+        if (startYolo == true) {
+
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
+
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
+
+            tinyYolo.setInput(imageBlob);
+
+
+            java.util.List<Mat> result = new java.util.ArrayList<Mat>(2);
+
+            List<String> outBlobNames = new java.util.ArrayList<>();
+            outBlobNames.add(0, "yolo_16");
+            outBlobNames.add(1, "yolo_23");
+
+            tinyYolo.forward(result, outBlobNames);
+
+
+            float confThreshold = 0.3f;
+
+
+            List<Integer> clsIds = new ArrayList<>();
+            List<Float> confs = new ArrayList<>();
+            List<Rect2d> rects = new ArrayList<>();
+
+
+            for (int i = 0; i < result.size(); ++i) {
+
+                Mat level = result.get(i);
+
+                for (int j = 0; j < level.rows(); ++j) {
+                    Mat row = level.row(j);
+                    Mat scores = row.colRange(5, level.cols());
+
+                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+
+
+                    float confidence = (float) mm.maxVal;
+
+
+                    Point classIdPoint = mm.maxLoc;
+
+
+                    if (confidence > confThreshold) {
+                        int centerX = (int) (row.get(0, 0)[0] * frame.cols());
+                        int centerY = (int) (row.get(0, 1)[0] * frame.rows());
+                        int width = (int) (row.get(0, 2)[0] * frame.cols());
+                        int height = (int) (row.get(0, 3)[0] * frame.rows());
+
+
+                        int left = centerX - width / 2;
+                        int top = centerY - height / 2;
+
+                        clsIds.add((int) classIdPoint.x);
+                        confs.add(confidence);
+
+
+                        rects.add(new Rect2d(left, top, width, height));
+                    }
+                }
+            }
+            int ArrayLength = confs.size();
+
+            if (ArrayLength >= 1) {
+                // Apply non-maximum suppression procedure.
+                float nmsThresh = 0.2f;
+
+
+                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
+
+
+                Rect2d[] boxesArray = rects.toArray(new Rect2d[0]);
+
+                MatOfRect2d boxes = new MatOfRect2d(boxesArray);
+
+                MatOfInt indices = new MatOfInt();
+
+
+                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
+
+
+                // Draw result boxes:
+                int[] ind = indices.toArray();
+                for (int i = 0; i < ind.length; ++i) {
+
+                    int idx = ind[i];
+                    Rect2d box = boxesArray[idx];
+
+                    int idGuy = clsIds.get(idx);
+
+                    float conf = confs.get(idx);
+
+
+                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
+
+
+                    int intConf = (int) (conf * 100);
+
+
+                    Imgproc.putText(frame, cocoNames.get(idGuy) + " " + intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 255, 0), 2);
+
+
+                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+
+
+                }
+            }
+
+        }
+
+
+        if (startFaces == true) {
+
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
+
+
+            Mat imageBlob = Dnn.blobFromImage(frame, 1.0, new Size(300, 300), new Scalar(104.0, 177.0, 123.0), true, false, CvType.CV_32F);
+
+            detector.setInput(imageBlob); //set the input to network model
+            Mat detections = detector.forward(); //feed forward the input to the netwrok to get the output
+
+            int cols = frame.cols();
+            int rows = frame.rows();
+
+            double THRESHOLD = 0.55;
+
+
+            detections = detections.reshape(1, (int) detections.total() / 7);
+
+            Log.d("EXPERIMENT5:ROWS", detections.rows() + "");
+
+            for (int i = 0; i < detections.rows(); ++i) {
+
+
+                double confidence = detections.get(i, 2)[0];
+
+
+                Log.d("EXPERIMENT6", i + " " + confidence + " " + THRESHOLD);
+                if (confidence > THRESHOLD) {
+
+                    int left = (int) (detections.get(i, 3)[0] * cols);
+                    int top = (int) (detections.get(i, 4)[0] * rows);
+                    int right = (int) (detections.get(i, 5)[0] * cols);
+                    int bottom = (int) (detections.get(i, 6)[0] * rows);
+
+
+                    // Draw rectangle around detected object
+
+                    if (left < 0) {
+                        left = 0;
+                    }
+                    if (top < 0) {
+                        top = 0;
+                    }
+                    if (right < 0) {
+                        right = 0;
+                    }
+                    if (bottom < 0) {
+                        bottom = 0;
+                    }
+
+                    int xLim = frame.size(1);
+                    int yLim = frame.size(0);
+
+                    if (left >= xLim) {
+                        left = xLim - 2;
+                    }
+                    if (right >= xLim) {
+                        right = xLim - 2;
+                    }
+
+                    if (top >= yLim) {
+                        top = yLim - 2;
+                    }
+                    if (bottom >= yLim) {
+                        bottom = yLim - 2;
+                    }
+
+
+                    Imgproc.rectangle(frame, new Point(left, top), new Point(right, bottom), new Scalar(255, 255, 0), 2);
+
+
+                }
+            }
+        }
+
+
+        return frame;
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+        if (startYolo == true) {
+
+            String tinyYoloCfg = getApplicationContext().getFilesDir().getPath() + "/dnns/yolov3-tiny.cfg";
+            String tinyYoloWeights = getApplicationContext().getFilesDir().getPath() + "/dnns/yolov3-tiny.weights";
+
+            tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+
+
+        }
+
+        if (startFaces == true) {
+
+            String protoPath = getApplicationContext().getFilesDir().getPath() + "/dnns/deploy.prototxt";
+            String caffeWeights = getApplicationContext().getFilesDir().getPath() + "/dnns/res10_300x300_ssd_iter_140000.caffemodel";
+
+            detector = Dnn.readNetFromDarknet(protoPath, caffeWeights);
+
+
+        }
+
+    }
+
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "There's a problem!", Toast.LENGTH_SHORT).show();
+        } else {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null) {
+
+            cameraBridgeViewBase.disableView();
+        }
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "write storage permission granted", Toast.LENGTH_LONG).show();
+                copyFileOrDir("dnns");
+            } else {
+                Toast.makeText(this, "write storage permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
